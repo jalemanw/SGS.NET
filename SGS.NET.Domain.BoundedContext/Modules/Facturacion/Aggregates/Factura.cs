042faa5b49f16e7ef75a5a6466049d74ad13a623
Josué Vinicio Alemán Watters <jova889@gmail.com>
2015-05-16 18:14:26 -0600

    Estructura del proyecto

---
+++ SGS.NET.Domain.BoundedContext/Modules/Facturacion/Aggregates/Factura.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SGS.NET.Domain.BoundedContext.Modules.Facturacion.Aggregates
+{
+    class Factura 
+    {
+    }
+}
